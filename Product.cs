774b48f685fb452c9d412cd161ddfbb7fa6a45d7
Johan Svensson <85johsve@skola.boras.se>
2022-10-04 09:36:16 +0200

    09:36

--- Product.cs
+++ Product.cs
@@ -1,3 +1,18 @@
 namespace choco;
+
+
+public class Product
+{
+    public int Price {get; set;}
+
+    public Product(int price);
+    {
+        Price = price;
+
+    }
+
+
+
+}
 //Chocolate
 //Hats
